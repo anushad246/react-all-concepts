97691b9e14bfc7ea20eb13bdb1da600efecd514c
AzureAD\AnushaDammalapati <anushadammalapati@aimlytics.ai>
2024-03-18 14:24:40 +0530

    Displaying data and Adding styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 
 import './App.css';
-import MyButton  from './practice-components/MyButton';
+// import MyButton  from './practice-components/MyButton';
+
+const user = {
+  name: 'Anusha Dammalapati',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: '90'
+}
 
 
 
@@ -8,7 +14,9 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to my app</h1>
-      <MyButton/>
+      <h3>name: {user.name}</h3>
+      <img className='avatar ' src={user.imageUrl} style={{height: user.imageSize, width:user.imageSize}} alt={'image of' + user.name}/>
+      {/* <MyButton/> */}
     </div>
   );
 }
