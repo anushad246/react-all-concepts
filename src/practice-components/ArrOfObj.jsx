81894e6591252e213681783f1d93556879d99e0c
AzureAD\AnushaDammalapati <anushadammalapati@aimlytics.ai>
2024-04-05 10:26:22 +0530

    updating array of object

--- src/practice-components/ArrOfObj.jsx
+++ src/practice-components/ArrOfObj.jsx
@@ -1,28 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ArrOfObj = () => {
+  const [cars, setCars] = useState([]);
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("");
+  const [carModel, setCarModel] = useState("");
 
-    const [cars, setCars] = useState([]);
-    const [carYear, setCarYear] = useState(new Date.getFullYear());
-    const [carMake, setCarMake] = useState("");
-    const [carModel, setCarModel] = useState("");
+  function handleAddCar() {
+    const newCar = { year: carYear, make: carMake, model: carModel };
+    setCars((e) => [...e, newCar]);
+  }
 
-    function handleAddCar(){};
-    function handleRemoveCar(index){}
-    function handleYearChange(event){}
-    function handleMakeChange(event){}
-    function handleModelChange(event){}
+  function handleRemoveCar(index) {}
+  function handleYearChange(event) {
+    setCarYear(event.target.value);
+  }
+  function handleMakeChange(event) {
+    setCarMake(event.target.value);
+  }
+  function handleModelChange(event) {
+    setCarModel(event.target.value);
+  }
 
   return (
     <div>
-        <h2>List of car objects</h2>
-        <ul></ul>
-        <input type='number' value={carYear} onChange={(e)=>handleYearChange(e)} placeholder='enter car make year'></input>
-        <input type='text' value={carMake} onChange={(e)=>handleMakeChange(e)} placeholder='enetr car make'></input>
-        <input type='text' value={carModel} onChange={(e)=>handleModelChange(e)} placeholder='enetr car model'></input>
-        <button onClick={handleAddCar}>Add Car</button>
+      <h2>List of car objects</h2>
+      <ul>
+        {cars.map((item, index) => (
+          <li key={index}>
+            {item.year}
+            {item.make}
+            {item.model}
+          </li>
+        ))}
+      </ul>
+      <input
+        type="number"
+        value={carYear}
+        onChange={(e) => handleYearChange(e)}
+        placeholder="enter car make year"
+      ></input>
+      <input
+        type="text"
+        value={carMake}
+        onChange={(e) => handleMakeChange(e)}
+        placeholder="enetr car make"
+      ></input>
+      <input
+        type="text"
+        value={carModel}
+        onChange={(e) => handleModelChange(e)}
+        placeholder="enetr car model"
+      ></input>
+      <button onClick={handleAddCar}>Add Car</button>
     </div>
-  )
-}
+  );
+};
 
-export default ArrOfObj
+export default ArrOfObj;
